e5f1699c4441bf21b7b4fa9c7aa219842a2fa651
Shashwat Poudel <shashwatp07@gmail.com>
2022-10-21 01:29:12 +0545

    minor bug fixes

--- main/views.py
+++ main/views.py
@@ -14,7 +14,8 @@ class HomeView(ListView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all()
+        context['posts'] = reversed(Post.objects.all())
+        print(context['posts'])
         return context
 
 class PostCreateView(CreateView):
