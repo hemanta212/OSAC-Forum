689e7826b07b1dc3c804d87ae3dcb59a85ffc01d
hemanta212 <sharmahemanta.212@gmail.com>
2022-11-09 22:56:23 +0545

    use poetry to install

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 # exit on error
 set -o errexit
 
-python -m pip install -U pip setuptools wheel 
-python -m pip install -r requirements.txt
+poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
